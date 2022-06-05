2a01bb6ca12b7e86545fcfb1558c2fcfbdc85738
Dancingkoalaa <tim2001.tw@gmail.com>
2022-06-05 23:05:06 +0200

    2ndcommit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Why is it always the same trainer that kicks me out!!')}
+      />
     </View>
   );
 }
